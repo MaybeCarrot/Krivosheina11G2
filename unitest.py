c9d141a606d6ad5a1bd2df752a215357f7409c98
AveRageVXC <vladislav.osokin.2005@mail.ru>
2022-03-10 14:19:45 +0300

    smth with db

--- unitest.py
+++ unitest.py
@@ -12,22 +12,22 @@ class DataBase:
     def __del__(self):
         self.__connection.close()
 
-    def goods(self, id: int):
-        request = 'SELECT(SELECT name FROM types WHERE id = type_id ) AS type_name,(SELECT name FROM goods WHERE id = :good_id) AS good_name,price FROM price'
-        return self.__cursor.execute(request, {'good_id': id}).fetchall()
+    def goods(self, id2: int):
+        request = f'SELECT(SELECT name FROM types WHERE id = {id2} ) AS type_name,(SELECT name FROM goods WHERE id = {id2}) AS good_name,price FROM price'
+        return self.__cursor.execute(request).fetchall()
 
-    def userBorn(self, id: int):
-        request = 'SELECT name, sername, (SELECT name FROM goods WHERE id = streams.good_id) AS good_name, date  FROM client WHERE born >= "2003/01/01" '
-        return self.__cursor.execute(request, {'born': id}).fetchall()
+    def userBorn(self, id1: int):
+        request = f'SELECT name, surname, (SELECT name FROM goods WHERE id = {id1}) AS good_name, date  FROM client WHERE born >= 2003-01-01 '
+        return self.__cursor.execute(request).fetchall()
     
 
-    def type(self, id: int):
-        request = 'SELECT name, kol FROM goods WHERE type_id = (SELECT id FROM types WHERE name = :type_name) '
-        return self.__cursor.execute(request, {'type_name': id}).fetchall()
+    def type(self, id3: int):
+        request = f'SELECT name, kol FROM goods WHERE type_id = {id3} '
+        return self.__cursor.execute(request).fetchall()
     
-    def priceDates(self, id: int):
-        request = 'SELECT(SELECT name FROM goods WHERE id = streams.good_id) AS good_name, price FROM price WHERE date >= :dates;'
-        return self.__cursor.execute(request, {'dates': id}).fetchall()
+    def priceDates(self, id4: int):
+        request = f'SELECT(SELECT name FROM goods WHERE id = {id4}) AS good_name, price FROM price WHERE date >= 1111-11-11 '
+        return self.__cursor.execute(request).fetchall()
 
     @property
     def get_cursor(self):
@@ -46,30 +46,37 @@ conn.close()
 class Test(unittest.TestCase):
     
     def test_goods(self):
+        print("tekst")
         for i in range(1, 4 + 1):
-            request = self.__temp.goods(id=i)
+            request = self.__temp.goods(id2=i)
+            print(request)
+
             self.assertEqual(1, len(request))
+            
 
     def test_userBorn(self):
+        print("tekst2")
         for i in range(1, 3 + 1):
-            request = self.__temp.userBorn(id=i)
+            request = self.__temp.userBorn(id1=i)
             self.assertEqual(1, len(request))
 
     def test_type(self):
+        print("tekst3")
         for i in range(1, 3 + 1):
-            request = self.__temp.type(id=i)
+            request = self.__temp.type(id3=i)
             self.assertEqual(1, len(request))
 
     def test_priceDates(self):
+        print("tekst4")
         for i in range(1, 3 + 1):
-            request = self.__temp.priceDates(id=i)
+            request = self.__temp.priceDates(id4=i)
             self.assertEqual(1, len(request))
 
     def tearDown(self):
         self.__temp.conn.close()
         
-    def Set(self):
-        self.__temp = DataBase(':memory:')
+    def setUp(self):
+        self.__temp = DataBase('1.db')
         self.__temp.get_cursor.executescript(
             '''
             CREATE TABLE IF NOT EXISTS "Client"(
@@ -81,11 +88,17 @@ class Test(unittest.TestCase):
                 PRIMARY KEY("id" AUTOINCREMENT)
             );
             
+            INSERT INTO Client VALUES(1, 'A', 'A', '2222-22-22', '1111-11-11');
+            INSERT INTO Client VALUES(2, 'B', 'B', '2222-22-22', '1111-11-11');
+            
             CREATE TABLE IF NOT EXISTS "Types"(
                 "id" INTEGER UNIQUE NOT NULL,
                 "name" TEXT NOT NULL,
                 PRIMARY KEY("id" AUTOINCREMENT)
             );
+            
+            INSERT INTO Types VALUES(1, 'A');
+            INSERT INTO Types VALUES(1, 'B');
             
             CREATE TABLE IF NOT EXISTS "Goods"(
                 "id" INTEGER UNIQUE,
@@ -95,6 +108,11 @@ class Test(unittest.TestCase):
                 FOREIGN KEY("type_id") REFERENCES "Types"("id"),
                 PRIMARY KEY("id" AUTOINCREMENT)
             );
+            
+            INSERT INTO Goods VALUES(1, 1, 'A', 14);
+            INSERT INTO Goods VALUES(2, 1, 'B', 14);
+            INSERT INTO Goods VALUES(3, 2, 'A', 14);
+            INSERT INTO Goods VALUES(4, 2, 'B', 14);
             
             CREATE TABLE IF NOT EXISTS "Operation"(
                 "id" INTEGER UNIQUE,
@@ -110,12 +128,18 @@ class Test(unittest.TestCase):
                 PRIMARY KEY("id" AUTOINCREMENT)
             );
             
+            INSERT INTO Operation VALUES(1, 1, 1, 1, 2, '1122-11-22', '2211-22-11');
+            
             CREATE TABLE IF NOT EXISTS "Price"(
                 "good_id" INTEGER NOT NULL,
                 "price" INTEGER NOT NULL,
                 "date" DATE NOT NULL DEFAULT'2000-00-00',
                 FOREIGN KEY("good_id") REFERENCES "Goods"("id")
             );
+            
+            INSERT INTO Prise VALUES(1, 12, '1111-11-11');
+            INSERT INTO Prise VALUES(2, 300, '1111-11-11');
+            INSERT INTO Prise VALUES(4, 790, '1111-11-11');
             
             '''
         )
